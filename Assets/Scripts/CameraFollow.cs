7a08403017546647747b71e50c21b68137691b17
Dilanga A.L.D <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2021-10-28 19:56:06 +0530

    JoystikTP

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform playerT;
+    private Vector3 camoffset;
+    [Range(0.01f, 1.0f)]
+    public float smfactor = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camoffset = transform.position - playerT.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newpos = playerT.position + camoffset;
+        transform.position = Vector3.Slerp(transform.position, newpos, smfactor);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walkspeed;
+    [SerializeField] private float movespeed;
+    // [SerializeField] private float jumpHeight;
+    [SerializeField] private bool isgrounded;
+    [SerializeField] private float groundcheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    protected Joystick joy;
+    private Animator anim;
+
+
+    private Vector3 moveforward;
+    private Vector3 velocity;
+
+    private CharacterController controller;
+
+
+
+    //calling functions in the begining of the game
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        joy = FindObjectOfType<Joystick>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    //calling functions in every frame of the game
+    private void Update()
+    {
+        Move();
+    }
+
+
+
+
+    //player's all movements
+    private void Move()
+    {
+        isgrounded = Physics.CheckSphere(transform.position, groundcheckDistance, groundMask);
+
+        if (isgrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float movez = joy.Vertical;
+        float movex = joy.Horizontal;
+        moveforward = new Vector3(movex, 0, movez).normalized;
+
+        if (moveforward != Vector3.zero)
+        {
+            Walk();
+            transform.forward = moveforward;
+            anim.SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
+        }
+
+        else if (moveforward == Vector3.zero)
+        {
+            Idle();
+            anim.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
+        }
+
+        /*
+                if (isgrounded)
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        Jump();
+                    }
+
+                }*/
+
+        controller.Move(moveforward * movespeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    //walk function 
+    private void Walk()
+    {
+
+        movespeed = walkspeed;
+    }
+
+
+
+    //idle function
+    private void Idle()
+    {
+        movespeed = 0;
+    }
+
+
+}
